8658fb10cf5b8e8ee0d287b4a3398210f97451d6
Scott Weber <scott@beeper.com>
2021-10-31 12:39:30 +0000

    Clean up how list of room members is updated
    Remove local state!

--- donutbot/bot.py
+++ donutbot/bot.py
@@ -19,18 +19,15 @@ def _str_to_int(s: str) -> Union[int, None]:
         return None
 
 class DonutBot(Plugin):
-    members: Dict[RoomID, List[SimpleMember]] = dict()
-    current_donuts: Dict[RoomID, Donut] = dict()
-    last_donuts: Dict[RoomID, Donut] = dict()
+    proposed_donuts: Dict[RoomID, Donut] = dict()
 
-    async def update_members(self, evt: MessageEvent) -> List[SimpleMember]:
+    async def get_members(self, evt: MessageEvent) -> List[SimpleMember]:
         room_id = evt.room_id
-        self.members[room_id] = []
         new_members = await evt.client.get_members(room_id)
         def state_event_to_simple_member(s: StateEvent) -> SimpleMember:
             return SimpleMember(display_name=s.content.displayname, mxid=s.state_key) # type: ignore
-        self.members[room_id] = [state_event_to_simple_member(s) for s in new_members if s.state_key != evt.client.mxid]
-        return self.members[room_id]
+        simple_members = [state_event_to_simple_member(s) for s in new_members if s.state_key != evt.client.mxid]
+        return simple_members
 
     @command.new(name="donut", require_subcommand=True)
     async def base_command(self):
@@ -38,8 +35,7 @@ class DonutBot(Plugin):
 
     @base_command.subcommand(help="List members in THE DONUT")
     async def list(self, evt: MessageEvent) -> None:
-        await self.update_members(evt)
-        members = self.members.get(evt.room_id)
+        members = await self.get_members(evt)
         if members:
             await evt.respond("Members in THE DONUT:\n" + _format_members(members))
         else:
@@ -48,17 +44,13 @@ class DonutBot(Plugin):
     @base_command.subcommand(help="Start a new DONUT")
     async def new(self, evt: MessageEvent) -> None:
         room_id = evt.room_id
-        current_donut = self.current_donuts.get(room_id)
-        last_donut = self.last_donuts.get(room_id)
-        if current_donut:
-            self.last_donuts[room_id] = current_donut
 
     @base_command.subcommand(help="Generate a sample DONUT")
     @command.argument("group_size", required=False, parser=_str_to_int)
     async def sample(self, evt: MessageEvent, group_size: Union[int, None] = None) -> None:
         group_size = group_size if group_size != None else 2
         # pyright gets it right, but mypy thinks group_size is optional here, so ignore
-        d = _generate_donut(await self.update_members(evt), group_size) # type: ignore
+        d = _generate_donut(await self.get_members(evt), group_size) # type: ignore
         await evt.respond(_format_donut(d))
 
     @base_command.subcommand(help="Test overlap")
@@ -66,8 +58,9 @@ class DonutBot(Plugin):
     async def overlap(self, evt: MessageEvent, group_size: Union[int, None] = None) -> None:
         group_size = group_size if group_size != None else 2
         # pyright gets it right, but mypy thinks group_size is optional here, so ignore
-        d1 = _generate_donut(await self.update_members(evt), group_size) # type: ignore
-        d2 = _generate_donut(await self.update_members(evt), group_size) # type: ignore
+        members = await self.get_members(evt)
+        d1 = _generate_donut(members, group_size) # type: ignore
+        d2 = _generate_donut(members, group_size) # type: ignore
         overlap = _are_donuts_overlapping(d1, d2)
         await evt.respond(_format_donut(d1))
         await evt.respond(_format_donut(d2))
